33030b4d3b5a4cf3dee55fe9d61aeccae2ff1f49
ctih1 <onni09nevala@gmail.com>
2026-07-02 00:45:46 +0300

    Improved UI

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :codely, Codely.Repo,
-  database: "database.db",
-  pool_size: 10
+  database: "data/database.db",
+  pool_size: 1
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
@@ -14,7 +14,7 @@ config :codely, Codely.Repo,
 config :codely, CodelyWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {192, 168, 32, 144}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
--- priv/repo/migrations/20250824084958_fix_sample.exs
+++
@@ -1,7 +0,0 @@
-defmodule Codely.Repo.Migrations.RenameAiSamlpeToAiSample do
-  use Ecto.Migration
-
-  def change do
-    rename table(:snippets), :ai_samlpe, to: :ai_sample
-  end
-end
