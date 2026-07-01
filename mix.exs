96696cfe298a862f1b95680e8b5e2e4045062b18
ctih1 <onni09nevala@gmail.com>
2026-07-01 18:00:23 +0300

    comma

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Codely.MixProject do
       {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
-      {:ecto_sqlite3, "~> 0.21.0"}
+      {:ecto_sqlite3, "~> 0.21.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
